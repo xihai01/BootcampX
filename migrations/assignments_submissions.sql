5bee46c82f41cc7b27c3733e5afb793b2e1bfe3a
Xihai Luo <xihai.luo01@gmail.com>
2021-11-22 22:27:31 +0000

    create tables for assignments and assignment_submissions with fake data

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,18 @@
+-- create table for assignments
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+-- create table for assignment submissions
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
